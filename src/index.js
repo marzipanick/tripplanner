aa4d1a655601369a4970079f40078c5f3fa27ea5
btan <btan7645@gmail.com>
2019-03-11 11:45:56 -0400

    i see the map!

---
+++ src/index.js
@@ -0,0 +1,12 @@
+console.log('Hey buddy!');
+
+const mapboxgl = require('mapbox-gl');
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2llbHRhbiIsImEiOiJjanQ0aWZpZ2gxNHN0M3pwcXdwa3RrbGVjIn0.K7xtf_HIobScvVFpm8zmAA';
+
+const map = new mapboxgl.Map({
+	container: 'map',
+	center: [ -74.009, 40.705 ], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+	zoom: 12, // starting zoom
+	style: 'mapbox://styles/mapbox/streets-v10' // mapbox has lots of different map styles available.
+});
